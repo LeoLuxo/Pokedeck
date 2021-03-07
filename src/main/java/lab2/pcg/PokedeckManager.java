ca94b497f3b313e7ca07243be56f509d00f2b5f7
LeoLuxo <leo@gamboa.eu>
2021-03-07 13:38:08 +0100

    Add saveDeck and loadDeck functionality

--- src/main/java/lab2/pcg/PokedeckManager.java
+++ src/main/java/lab2/pcg/PokedeckManager.java
@@ -1,26 +1,53 @@
 package lab2.pcg;
 
+import com.google.gson.Gson;
 import lab2.pcg.deck.CardDeck;
+
+import java.io.*;
 
 
 public class PokedeckManager {
 	
 	private static final String savePath = "pokedecks/";
+	private static final String extention = ".json";
 	
 	public static boolean deckExists(String name) {
-		return false;
+		return getJsonFile(name).canRead();
 	}
 	
 	public static CardDeck loadDeck(String name) {
-		return new CardDeck();
+		try {
+			Gson gson = new Gson();
+			FileReader reader = new FileReader(getJsonFile(name));
+			return gson.fromJson(reader, CardDeck.class);
+			
+		} catch (FileNotFoundException ex) {
+			// If the file can't be found it means that the interface failed horribly
+			ex.printStackTrace();
+		}
+		
+		return null;
 	}
 	
 	public static void saveDeck(CardDeck deck) {
-		
+		try {
+			FileWriter fw = new FileWriter(getJsonFile(deck.name));
+			Gson gson = new Gson();
+			fw.write(gson.toJson(deck));
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+	
+	
 	
 	private static String sanitizeInput(String input) {
 		return input.toLowerCase().strip();
+	}
+	
+	private static File getJsonFile(String name) {
+		return new File(savePath + name + extention);
 	}
 	
 }
--- src/main/java/lab2/pcg/deck/CardDeck.java
+++ src/main/java/lab2/pcg/deck/CardDeck.java
@@ -1,4 +1,9 @@
 package lab2.pcg.deck;
 
 public class CardDeck {
+	public String name;
+	
+	public CardDeck(String name) {
+	
+	}
 }
