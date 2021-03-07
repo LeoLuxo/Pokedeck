b411f2591c66c0009fae96a808a71fc24d0d5f3b
LeoLuxo <leo@gamboa.eu>
2021-03-07 15:03:03 +0100

    Add Card and CardDeck add;delete;update functions

--- src/main/java/lab2/pcg/deck/CardDeck.java
+++ src/main/java/lab2/pcg/deck/CardDeck.java
@@ -1,9 +1,28 @@
 package lab2.pcg.deck;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class CardDeck {
 	public transient String name;
 	
+	private List<Card> cards = new ArrayList<>();
+	
 	public CardDeck(String name) {
 		this.name = name;
+	}
+	
+	public void addCard(Card card) {
+		cards.add(card);
+	}
+	
+	public void removeCard(int index) {
+		cards.remove(index);
+	}
+	
+	public void updateCard(int index, Card card) {
+		cards.remove(index);
+		cards.add(index, card);
 	}
 }
