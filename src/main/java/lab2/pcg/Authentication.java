02f686d1146f7fdbaea8718e301ba2b253a42042
LeoLuxo <leo@gamboa.eu>
2021-03-07 13:49:51 +0100

    Fix Authentication to use global scanner

--- src/main/java/lab2/pcg/Authentication.java
+++ src/main/java/lab2/pcg/Authentication.java
@@ -6,23 +6,22 @@ import lab2.pcg.PokedeckManager;
 public class Authentication  {
 
   public static void askName() {
-    Scanner scanner = new Scanner(System.in);
     // Ask user for name
     System.out.println("What's your name?");
-    String name = scanner.nextLine();
+    String name = Pokedeck.scanner.nextLine();
     // Call deckExists method from PokedeckManager class to see if it exists.
     //It doesn't exit- call method to ask to create one
     if (PokedeckManager.deckExists(name)) {
-      askNewDeck(scanner);
+      askNewDeck();
     }
     else {
     //go to main menu
     }
    }
 
-  public static void askNewDeck(Scanner scanner) {
+  public static void askNewDeck() {
     System.out.println("Would you like to create a new Deck?y/n");
-    String answer = scanner.nextLine();
+    String answer = Pokedeck.scanner.nextLine();
     if (answer =="y") {
       //call Create new deck method
     }
