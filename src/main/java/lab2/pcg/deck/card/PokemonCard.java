28c7fe8eece19232d70bb0b1daac5eb866138034
LeoLuxo <leo@gamboa.eu>
2021-03-08 17:41:02 +0100

    Finish PokemonCard structure and Add Expansion enum

--- src/main/java/lab2/pcg/deck/card/PokemonCard.java
+++ src/main/java/lab2/pcg/deck/card/PokemonCard.java
@@ -1,9 +1,35 @@
 package lab2.pcg.deck.card;
 
+import lab2.pcg.deck.enums.Expansion;
+
+
 public class PokemonCard extends Card {
 	
-	public PokemonCard(String name, String description) {
+	public int hp;
+	public int stage;
+	public String evolvesFrom;
+	public int cardNumber;
+	public Expansion expansionSymbol;
+	
+	public PokemonCard(String name, String description, int hp, int stage, String evolvesFrom, int cardNumber, Expansion expansionSymbol) {
 		super(name, description);
+		this.hp = hp;
+		this.stage = stage;
+		this.evolvesFrom = evolvesFrom;
+		this.cardNumber = cardNumber;
+		this.expansionSymbol = expansionSymbol;
+	}
+	
+	public PokemonCard(String name, String description, int hp, int stage, String evolvesFrom, int cardNumber) {
+		this(name, description, hp, stage, evolvesFrom, cardNumber, Expansion.UNDEFINED);
+	}
+	
+	public PokemonCard(String name, String description, int hp, int stage, String evolvesFrom) {
+		this(name, description, hp, stage, evolvesFrom, -1);
+	}
+	
+	public PokemonCard(String name, String description, int hp) {
+		this(name, description, hp, 0, "");
 	}
 	
 }
