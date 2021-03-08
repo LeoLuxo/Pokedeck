e79fe4f9dc2a329a1b0caff72b7df2ff4692af48
LeoLuxo <leo@gamboa.eu>
2021-03-08 22:02:22 +0100

    Add filter methods in CardDeck

--- src/main/java/lab2/pcg/deck/CardDeck.java
+++ src/main/java/lab2/pcg/deck/CardDeck.java
@@ -1,8 +1,12 @@
 package lab2.pcg.deck;
 
-import lab2.pcg.deck.card.Card;
+import lab2.pcg.deck.card.*;
+
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 
 public class CardDeck {
@@ -25,5 +29,39 @@ public class CardDeck {
 	public void updateCard(int index, Card card) {
 		cards.remove(index);
 		cards.add(index, card);
+	}
+	
+	public List<Card> filterCards(Predicate<Card> filter, Comparator<Card> sorter) {
+		return cards.stream()
+				.filter(filter)
+				.sorted(sorter)
+				.collect(Collectors.toList());
+	}
+	
+	public List<Card> filterPokemonCards(Predicate<PokemonCard> filter, Comparator<PokemonCard> sorter) {
+		return cards.stream()
+				.filter(card -> card instanceof PokemonCard)
+				.map(card -> (PokemonCard) card)
+				.filter(filter)
+				.sorted(sorter)
+				.collect(Collectors.toList());
+	}
+	
+	public List<Card> filterTrainerCards(Predicate<TrainerCard> filter, Comparator<TrainerCard> sorter) {
+		return cards.stream()
+				.filter(card -> card instanceof TrainerCard)
+				.map(card -> (TrainerCard) card)
+				.filter(filter)
+				.sorted(sorter)
+				.collect(Collectors.toList());
+	}
+	
+	public List<Card> filterEnergyCards(Predicate<EnergyCard> filter, Comparator<EnergyCard> sorter) {
+		return cards.stream()
+				.filter(card -> card instanceof EnergyCard)
+				.map(card -> (EnergyCard) card)
+				.filter(filter)
+				.sorted(sorter)
+				.collect(Collectors.toList());
 	}
 }
