3d3be6bea78320683f87a9612d347aa25970bdef
LeoLuxo <leo@gamboa.eu>
2021-03-09 09:16:26 +0100

    Add EnergyType to PokemonCard

--- src/main/java/lab2/pcg/deck/card/PokemonCard.java
+++ src/main/java/lab2/pcg/deck/card/PokemonCard.java
@@ -1,19 +1,22 @@
 package lab2.pcg.deck.card;
 
+import lab2.pcg.deck.enums.EnergyType;
 import lab2.pcg.deck.enums.Expansion;
 
 
 public class PokemonCard extends Card {
 	
 	public int hp;
-	public int stage = 0;
-	public String evolvesFrom = "";
+	public int stage;
+	public String evolvesFrom;
+	public EnergyType type;
 	
-	public PokemonCard(String name, String description, int cardNumber, Expansion expansionSymbol, int hp, int stage, String evolvesFrom) {
+	public PokemonCard(String name, String description, int cardNumber, Expansion expansionSymbol, int hp, int stage, String evolvesFrom, EnergyType type) {
 		super(name, description, cardNumber, expansionSymbol);
 		this.hp = hp;
 		this.stage = stage;
 		this.evolvesFrom = evolvesFrom;
+		this.type = type;
 	}
 	
 }
