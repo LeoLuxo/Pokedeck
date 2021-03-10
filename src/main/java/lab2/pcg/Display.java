6296e14810111c39db7224853ff26dc87e3cce46
LeoLuxo <leo@gamboa.eu>
2021-03-10 11:53:02 +0100

    Change Display with design printing

--- src/main/java/lab2/pcg/Display.java
+++ src/main/java/lab2/pcg/Display.java
@@ -5,6 +5,11 @@ import java.awt.*;
 
 public class Display {
 	
+	private static final String ESC = "\u001B";
+	private static final String CSI = ESC + "[";
+	
+	
+	
 	public static void initDisplay() {
 		enableAlternativeScreenBuffer();
 		disableLineWrap();
@@ -49,21 +54,41 @@ public class Display {
 	
 	public static void resetStyle() {printCSICode("0m");}
 	
-	public static void setForegroundColor(Color c) {setForegroundColor(c.getRed(), c.getGreen(), c.getBlue());}
-	public static void setBackgroundColor(Color c) {setBackgroundColor(c.getRed(), c.getGreen(), c.getBlue());}
-	public static void setForegroundColor(int r, int g, int b) {printCSICode("38;2;"+r+";"+g+";"+b+"m");}
-	public static void setBackgroundColor(int r, int g, int b) {printCSICode("48;2;"+r+";"+g+";"+b+"m");}
+	public static void setForegroundColor(Color c) {printCSICode(buildColorCode(c, false));}
+	public static void setBackgroundColor(Color c) {printCSICode(buildColorCode(c, true));}
+	public static void setForegroundColor(int r, int g, int b) {printCSICode(buildColorCode(r, g, b, false));}
+	public static void setBackgroundColor(int r, int g, int b) {printCSICode(buildColorCode(r, g, b, true));}
 	
 	
 	
+	public static void printColorDesign(String design, Color highlightColor) {
+		design = design
+				.replaceAll("€", buildColorCode(highlightColor, false))
+				.replaceAll("ç", buildColorCode(highlightColor, true))
+				.replaceAll("§", CSI);
+		System.out.print(design);
+		System.out.flush();
+	}
+	
+	
+	
+	// System.out only automatically flushes on newline characters, so we manually flush
 	private static void printESCCode(String code) {
-		System.out.print("\u001B" + code);
+		System.out.print(ESC + code);
 		System.out.flush();
 	}
 	
 	private static void printCSICode(String code) {
-		System.out.print("\u001B[" + code);
+		System.out.print(CSI + code);
 		System.out.flush();
+	}
+	
+	private static String buildColorCode(Color color, boolean background) {
+		return buildColorCode(color.getRed(), color.getGreen(), color.getBlue(), background);
+	}
+	
+	private static String buildColorCode(int r, int g, int b, boolean background) {
+		return background ? "48;2;"+r+";"+g+";"+b+"m" : "38;2;"+r+";"+g+";"+b+"m";
 	}
 	
 }
