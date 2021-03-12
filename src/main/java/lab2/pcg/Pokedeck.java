b92c211d56df05807646002be7c9ab75a4115f55
LeoLuxo <leo@gamboa.eu>
2021-03-12 11:39:07 +0100

    Add start comment

--- src/main/java/lab2/pcg/Pokedeck.java
+++ src/main/java/lab2/pcg/Pokedeck.java
@@ -8,6 +8,8 @@ public class Pokedeck {
   public static void main(String[] args) {
     
     try {
+      // The program uses ANSI escape codes extensively as well as the "stty" command.
+      // It will ONLY work on Linux terminals or WSL, and isn't guaranteed to work with every terminal.
       
       Display.initDisplay();
 
